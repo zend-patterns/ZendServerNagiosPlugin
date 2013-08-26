9c28883d376e3aec45be75f24aa8c632739ec7b9
sophpie <sophie.b@zend.com>
2013-08-26 17:25:32 +0200

    Adding auto creation of a shell shortcut for plugin command

--- deployment/NagiosPluginDeployer.php
+++ deployment/NagiosPluginDeployer.php
@@ -1,8 +1,22 @@
 <?php
-class NagiosPLuginDeployer extends \ZendDevoips\DepH\DeploymentListener
+use Zend\EventManager\Event;
+use ZendDevops\DepH\Deployment\Context;
+use ZendDevops\DepH\Deployment\DeploymentListener;
+
+class NagiosPluginDeployer extends DeploymentListener
 {
     public function postStageAction(Event $e)
     {
-        
+        //Create shell command shortcut
+        switch ($this->getContext()->getWebServerType())
+        {
+            case Context::WEBSERVER_TYPE_APACHE:
+                $bashPath = '/usr/local/zend/./nagiosplugin.sh';
+                $templateDir = $this->getContext()->getDeploymentSrc() . '/template/';
+                $template = $this->getTemplateManager()->getTemplate($templateDir . 'linuxBash.phtml');
+                $template->appDir = $this->getContext()->getApplicationBaseDir();
+                $template->render($bashPath);
+            break;
+        }
     }
 }
