8b78032daf6b322d78dbc54bdb81d801d863e379
sophpie <sophie.b@zend.com>
2013-08-29 09:38:40 +0200

    Clean it up...

--- deployment/post_stage.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'ZendDevops/init_autoloading.php';
-require_once 'NagiosPluginDeployer.php';
-
-$depH = new NagiosPluginDeployer('nagios.deployment.config.php');
-$depH->run();
--- init_autoloader.php
+++ init_autoloader.php
@@ -16,32 +16,24 @@
  * most users, however, feel free to configure autoloading however you'd like.
  */
 
-// Composer autoloading
-if (file_exists('vendor/autoload.php')) {
-    $loader = include 'vendor/autoload.php';
-}
-
 $zf2Path = false;
 
-if (is_dir('vendor/ZF2/library')) {
-    $zf2Path = 'vendor/ZF2/library';
-} elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
+if (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
     $zf2Path = get_cfg_var('zf2_path');
 }
 
 if ($zf2Path) {
-    if (isset($loader)) {
-        $loader->add('Zend', $zf2Path);
-    } else {
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        Zend\Loader\AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true
-            )
-        ));
-    }
+    include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+    Zend\Loader\AutoloaderFactory::factory(array(
+        'Zend\Loader\StandardAutoloader' => array(
+            'autoregister_zf' => true
+        ),
+        'namespace' => array(
+            'ZendServerWebApi' => __DIR__ . '/vendor/ZendServerWebApi/src',
+        ),
+    ));
 }
 
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
