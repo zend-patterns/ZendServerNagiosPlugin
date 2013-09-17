e19be77822f9322bf11fd9fcb289314ab1c7dfd3
sophpie <sophie.b@zend.com>
2013-09-17 16:36:41 +0200

    ignore vendor

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,25 +1,9 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-/**
- * This autoloading setup is really more complicated than it needs to be for most
- * applications. The added complexity is simply to reduce the time it takes for
- * new developers to be productive with a fresh skeleton. It allows autoloading
- * to be correctly configured, regardless of the installation method and keeps
- * the use of composer completely optional. This setup should work fine for
- * most users, however, feel free to configure autoloading however you'd like.
- */
-
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    $loader = include 'vendor/autoload.php';
+}
 $zf2Path = false;
-
-if (file_exists('win.zf2.conf.php')) include 'win.zf2.conf.php';
-
 if (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
@@ -27,17 +11,20 @@ if (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or g
 }
 
 if ($zf2Path) {
-    include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-    Zend\Loader\AutoloaderFactory::factory(array(
-        'Zend\Loader\StandardAutoloader' => array(
-            'autoregister_zf' => true,
-            'namespace' => array(
-                    'ZendServerWebApi' => __DIR__ . '/vendor/ZendServerWebApi/src',
+    if (isset($loader)) {
+        $loader->add('Zend', $zf2Path);
+    } else {
+        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+        Zend\Loader\AutoloaderFactory::factory(array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'autoregister_zf' => true,
+                'namespace' => array(
+                        'ZendServerWebApi' => __DIR__ . '/vendor/ZendServerWebApi/src',
+                ),
             ),
-        ),
-    ));
+        ));
+    }
 }
-
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
 }
--- module/ZendServerNagiosPlugin/src/ZendServerNagiosPlugin/Controller/FrontendController.php
+++ module/ZendServerNagiosPlugin/src/ZendServerNagiosPlugin/Controller/FrontendController.php
@@ -6,6 +6,8 @@ use Zend\Config\Config;
 use Zend\Console\ColorInterface;
 use Zend\Console\Prompt\Line;
 use Zend\Console\Prompt\Select;
+use Zend\Config\Reader\Ini;
+use Zend\Config\Factory;
 /**
  * Nagios plugins controller
  */
@@ -18,20 +20,19 @@ class FrontendController extends AbstractNagiosController
      */
     public function installAction()
     {
-        //Set up web api key.
-        $zsurl = Line::prompt('Zend Server url : ',false,100);
-        $zskey = Line::prompt('Zend Server web API key name : ',false,100);
-        $zssecret = Line::prompt('Zend Server web API key secret : ',false,100);
-        $zsversion = Select::prompt('Zend Server version : ',
-                array(
+        $availableZsVersions = array(
                     '0' => '5.1',
                     '1' => '5.5',
                     '2' => '5.6',
                     '3' => '6.0',
                     '4' => '6.1'
-                ),
-                false,false
-       );
+                );
+        //Set up web api key.
+        $zsurl = Line::prompt('Zend Server url : ',false,100);
+        $zskey = Line::prompt('Zend Sephprver web API key name : ',false,100);
+        $zssecret = Line::prompt('Zend Server web API key secret : ',false,100);
+        $zsversion = Select::prompt('Zend Server version : ',$availableZsVersions,false,false);
+        $zsversion = $availableZsVersions[$zsversion];
         $config = new Config(array(
                 'zsapi' => array(
                     'default_target' => array(
@@ -47,10 +48,13 @@ class FrontendController extends AbstractNagiosController
         $localConfigFile = __DIR__ .'/../../../../../config/autoload/zsapi.local.php';
         $configWritter->toFile($localConfigFile, $config);
         $this->consoleWriteLn('[OK]', ColorInterface::GREEN);
+    }
+    
+    /**
+     * NRPE Configuration for Linux environment
+     */
+    protected function nrpeConfLinux()
+    {
         
-        //Set up Nagios file - nrpe.cfg
-        
-        
-        return false;
     }
 }
