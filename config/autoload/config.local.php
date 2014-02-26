334e576c0f68bd389da8eec30a4738c62fdee39a
sophie <sophie@sophpie>
2014-02-26 13:24:49 +0100

    Fixing default value in deployment.xml. Adding http client config

--- config/autoload/config.local.php
+++ config/autoload/config.local.php
@@ -5,9 +5,15 @@ return array(
 			'zsurl' => 'http://localhost:10081/ZendServer',
 			'zskey' => 'nagios',
 			'zssecret' => 'bbf577555266b24542748d05b892e236d8ee9df0bf864fd9a1a126e5bdaf5d07',
-			'zsversion' => '6.2'
+			'zsversion' => '6.3'
 		),
 	),
+    'api_http_client' => array(
+    		'class' => 'ZendServerWebApi\Model\Http\Client',
+    		'config' => array(
+    				'adapter'=> 'ZendServerWebApi\Model\Http\Adapter\Socket'
+    		),
+    ),
 	'nagios' => array(
 		'plugin' => array(
 			'directory' => '/var/www/ZendServerNagiosPlugins'
